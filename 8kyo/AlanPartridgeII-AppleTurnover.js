3602f6a8ec9f95d13f32a9d2319c5aa303a5a4e0
mr.Seven <mr.upgrade3@protonmail.com>
2023-05-16 08:39:02 +0300

    Load solutions for 2 katas

---
+++ 8kyo/AlanPartridgeII-AppleTurnover.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/580a094553bd9ec5d800007d/train/javascript
+
+"use strict";
+
+const apple = (x) =>
+    Math.pow(+x, 2) > 1000
+        ? "It's hotter than the sun!!"
+        : "Help yourself to a honeycomb Yorkie for the glovebox.";

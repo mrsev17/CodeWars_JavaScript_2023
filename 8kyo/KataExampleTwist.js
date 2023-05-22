9f1c9bc40657e35faf5b7880e9c23f6d9e4e0d70
mr.Seven <mr.upgrade3@protonmail.com>
2023-05-22 08:33:37 +0300

    Add solution for 1 kata

---
+++ 8kyo/KataExampleTwist.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/525c1a07bb6dda6944000031/train/javascript
+
+"use strict";
+
+const websites = [];
+for (let i = 0; i <= 999; i += 1) {
+    websites.push("codewars");
+}
